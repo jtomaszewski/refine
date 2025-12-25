7c73db1088d5424c8a7da5986b2f9e7e16dc5970
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2025-12-25 18:14:40 +0300

    chore: more updates

--- documentation/asd.js
+++ documentation/asd.js
@@ -2,8 +2,32 @@ const fs = require("fs");
 
 const redirects = JSON.parse(fs.readFileSync("./redirects.json")).redirects;
 
-console.log(redirects);
+const addRedirect = (from, to, statusCode = 301) => {
+  if (to.at(-1) !== "/") {
+    to += "/";
+  }
 
-const lines = redirects.map((r) => `${r.from} ${r.to} 301`).join("\n\n");
+  return `${from} ${to} 301`;
+};
 
-fs.writeFileSync("redirects", lines);
+const lines = redirects
+  .flatMap((r) => {
+    if (typeof r.from === "string") {
+      return addRedirect(r.from, r.to);
+    }
+
+    return r.from.map((rr) => addRedirect(rr, r.to));
+  })
+  .sort();
+
+lines.push(
+  "/docs/next/* /core/docs/:splat 301",
+  "/docs/* /core/docs/:splat 301",
+  "/tutorial/* /core/tutorial/:splat",
+  "/templates /core/templates/ 301",
+  "/templates/* /core/templates/:splat 301",
+  "/templates/* /core/templates/:splat 301",
+  "/integrations /core/integrations/",
+);
+
+fs.writeFileSync("./static/_redirects", lines.join("\n\n"));
